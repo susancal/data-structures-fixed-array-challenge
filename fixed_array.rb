1df30ceb5af6d23f32c4ff283e7052b5344c77e0
Susie O'Brien <email": null>
2016-08-20 17:01:16 -0500

    get set and new methods

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,29 @@
 class FixedArray
+  attr_reader :arr
+
+  def initialize(size)
+    @arr = Array.new(size)
+  end
+
+  def get(index)
+    if index <= (@arr.length-1)
+      return @arr[index]
+    else
+      raise OutOfBoundException
+    end
+  end
+
+  def set(index, element)
+    if index <= (@arr.length-1)
+      @arr[index] = element
+    else
+      raise OutOfBoundException
+    end
+  end
 
 end
+
+f = FixedArray.new(5)
+f.set(3, "check")
+f.set(2, "check")
+p f.arr
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,17 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+  let(:fa) { FixedArray.new(4) }
+  it "is an array" do
+    expect(fa.arr).to eq([nil, nil, nil, nil])
+  end
+
+  it "sets an element at an index" do
+    fa.set(1, "testing!")
+    expect(fa.arr).to eq([nil, "testing!", nil, nil])
+  end
+
+  it "throws an error if you try to set an index out of bounds" do
+    expect(fa.set(8, "whoa there")).to raise_error
+  end
 end
